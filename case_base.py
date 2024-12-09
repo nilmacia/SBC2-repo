d08fac1d641e54a79db412eaaef172b0da018452
Nil Macià <nil.macia@gmail.com>
2024-12-09 22:57:13 +0100

    .

--- case_base.py
+++ case_base.py
@@ -4,11 +4,11 @@ d_weights = np.array([1,1,1,1,1,1,1,1,1])
 
 class Node:
     def __init__(self, thresholds=None, children=None):
-        if thresholds != None:
+        if thresholds is not None:
             self.thresholds = thresholds
             self.children = children
         else:
-            cases = None
+            self.cases = None
 
     def feed(self, case):
         if 'thresholds' in dir(self):
@@ -19,13 +19,13 @@ class Node:
                     if case[self.i] < th:
                         return child.feed(case)
         else:
-            if self.cases == None:
+            if self.cases is None:
                 self.cases = np.stack([case])
                 return
             else:
                 dist = np.sqrt(np.sum(((case - self.cases) * d_weights)**2, -1))
                 closest = self.cases[np.argmin(dist)]
-                self.cases = np.concat(self.cases, [case])
+                self.cases = np.concat([self.cases, [case]])
                 return closest
         
 layer_thresholds = [4, 30, 4, 2, 1, 1, 1, 1, 1]
@@ -33,7 +33,7 @@ layer_thresholds = [4, 30, 4, 2, 1, 1, 1, 1, 1]
 nodes = [Node() for _ in range(2**len(d_weights))]
 
 for i in reversed(range(len(d_weights))):
-    for _ in range(2**(i-1)):
+    for _ in range(int(2**(i-1))):
         node = Node(layer_thresholds[i], nodes[:2])
         nodes.append(node)
         del nodes[:2]
