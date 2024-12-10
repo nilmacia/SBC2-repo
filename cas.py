bcfc91b243bc95f11aa775bf0b90190adce26c9f
Nil Macià <nil.macia@gmail.com>
2024-12-10 10:17:59 +0100

    .

--- cas.py
+++ cas.py
@@ -1,4 +1,7 @@
 import json
+import numpy as np
+
+path_domini = "data/domini.json"
 
 class Cas:
     def __init__(self, nombre: int, edat: int, hores: int, dies: int,
@@ -11,15 +14,15 @@ class Cas:
         - autors: list[str]
         - estils: list[str]
         
-        Els atributs de "autors", "estils" i "obres" seran llistes d'strings
-        però es transformaran a vectors binaris amb el mètode "array".
-        Per determinar la llargada dels vectors s'utilitzarà el fitxer
-        "data/domini.json".
-        Si s'introdueixen valors no vistos abans s'inclouran al
-        fitxer però aleshores hem de tenir en compte que els vectors retornats
-        ja no seran compatibles amb els generats anteriorment.
-        Procurem utilitzar els valors ja inclosos mentre sigui posible.
+        Per construir els vectors binaris s'utilitza el fitxer data/domini.json.
+        Els autors i estils mai vistos s'afegiran (i per tant la mida dels
+        vetors augmentarà).
+        Intentem no fer innecessàriament gran el domini de moment.
         """
+
+        assert all(isinstance(x, int) for x in [nombre, edat, hores, dies])
+        assert all(isinstance(x, list) for x in [autors, estils])
+        assert all(isinstance(x, str) for x in autors + estils)
 
         self.nombre = nombre
         self.edat = edat
@@ -29,7 +32,7 @@ class Cas:
         self.estils = estils
         self.obres = None
 
-        with open("data/domini.json") as f:
+        with open(path_domini) as f:
             domini = json.load(f)
         
         autors = set(autors)
@@ -41,3 +44,60 @@ class Cas:
         estilsD = set(domini["estils"])
         if not estils <= estilsD:
             domini["estils"].extend([e for e in estils if e not in estilsD])
+        
+        with open(path_domini, 'w') as f:
+            json.dump(domini, f)
+
+    def recomanar(self, obres: list[str]) -> None:
+        """
+        Ens interessa més assignar-les com una llista d'strings o com el vector
+        binari??
+        Ja ho cambiarem
+
+        Per construir els vectors binaris s'utilitza el fitxer data/domini.json.
+        Les obres mai vistos s'afegiran (i per tant la mida dels
+        vetors augmentarà).
+        Intentem no fer innecessàriament gran el domini de moment.
+        """
+
+        assert isinstance(obres, list)
+        assert all(isinstance(x, str) for x in obres)
+
+        self.obres = obres
+
+        with open(path_domini) as f:
+            domini = json.load(f)
+
+        obres = set(obres)
+        obresD = set(domini["obres"])
+        if not obres <= obresD:
+            domini["obres"].extend([e for e in obres if e not in obresD])
+        
+        with open(path_domini, 'w') as f:
+            json.dump(domini, f)
+
+    def array_feats(self) -> np.ndarray:
+        """
+        Retorna el vector dels 4 atributs "nombre", "edat", "hores" i "dies";
+        seguit dels dos vectors binaris per "autors" i "estils"
+        """
+
+        atributs = np.array([self.nombre, self.edat, self.hores, self.dies])
+
+        with open(path_domini) as f:
+            domini = json.load(f)
+        
+        autors = np.isin(domini["autors"], self.autors).astype(int)
+        estils = np.isin(domini["estils"], self.estils).astype(int)
+        
+        return np.concat([atributs, autors, estils])
+
+    def array_obres(self) -> np.ndarray:
+        """
+        Retorna el vector binari per les obres recomanades
+        """
+
+        with open(path_domini) as f:
+            domini = json.load(f)
+        
+        return np.isin(domini["obres"], self.obres).astype(int)
