11489295d825317e68f689952742fb4671949f72
janbennassar <147800871+janbennassar@users.noreply.github.com>
2024-12-16 16:44:06 +0100

    hgfgd

--- cbr.py
+++ cbr.py
@@ -2,6 +2,8 @@ import numpy as np
 from base_casos import Node
 import json
 
+path_domini = "dades/domini.json"
+
 class CBR:
     def __init__(self, root, artist_weight=1.0, period_weight=1.0, age_weight=1.0, hours_weight=1.0):
         """
