329e2968e73af2f3660a3fcb65826d804bb6eeac
Nil Macià <nil.macia@gmail.com>
2024-12-16 19:26:10 +0100

    .

--- carregar_casos.py
+++
@@ -1,9 +0,0 @@
-import json
-import numpy as np
-
-# Format: [nombre, edat, hores, dies, preferències x5]
-
-def load():
-    with open("data/casos_sintetics.json") as f:
-        casos = json.load(f)
-    return np.array(casos)
